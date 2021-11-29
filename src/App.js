a221c95cb31ec298e82535314eb2e51b423c4668
HuzaifaTP <91236840+HuzaifaTP@users.noreply.github.com>
2021-11-29 12:19:02 +0800

    configs updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import config from './config';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+      <h1>{config.baseUrl}</h1>
       </header>
     </div>
   );
